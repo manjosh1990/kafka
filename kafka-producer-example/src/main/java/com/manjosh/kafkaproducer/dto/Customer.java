c450c8b5ec288d0f0b5a95c86dd2fa608a72a08d
Manjosh R <manjosh.1990@yahoo.com>
2024-09-01 10:01:06 +0530

    kafka implementations

---
+++ kafka-producer-example/src/main/java/com/manjosh/kafkaproducer/dto/Customer.java
@@ -0,0 +1,11 @@
+package com.manjosh.kafkaproducer.dto;
+
+import lombok.Data;
+
+@Data
+public class Customer {
+    private int id;
+    private String name;
+    private String email;
+    private String contactNo;
+}
